29e1659abd0e60875273f20078e2ab53d31b9048
Maryam <maryammer@icloud.com>
2018-04-26 10:52:45 +0200

    sam

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-script.js
+var sandwiches = { "sandwich": "hamburger", 
+                    "calories": "260" };
+
+var fries = { "fries_size": "Large French Fries",
+                 "calories": "570" };
+
+document.getElementById("java").innerHTML= " My favorite sandwich is a " + sandwiches.sandwich + " which has approximately " +sandwiches.calories+ " calories, along with it I enjoy eating " +fries.fries_size+ " which have about " + fries.calories + " calories.";
+
+
+document.getElementById("json").innerHTML= " My favorite sandwich is a " + sandwichess.sandwich + " which has approximately " +sandwichess.calories+ " calories, along with it I enjoy eating " +friess.fries_size+ " which have about " + friess.calories + " calories.";
+
+
